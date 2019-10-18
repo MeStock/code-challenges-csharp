e245af289f4a94c8ba31568ad22d1d004644e845
Melissa Stock <melissastock44@gmail.com>
2019-10-18 10:07:56 -0700

    initialize

---
+++ code-challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace code_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
