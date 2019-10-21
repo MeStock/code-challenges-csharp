c104c020999273951f4f1733ebe3f5f5c236a7f9
Melissa Stock <melissastock44@gmail.com>
2019-10-21 13:01:32 -0700

    Solved hackerrank grading student problem

---
+++ code-challenges/GradingStudents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace code_challenges
+{
+    /*
+
+HakerLand University has the following grading policy:
+
+Every student receives a  in the inclusive range from  to .
+Any  less than  is a failing grade.
+Sam is a professor at the university and likes to round each student's  according to these rules:
+
+If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+For example,  will be rounded to  but  will not be rounded because the rounding would result in a number that is less than .
+
+Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
+
+Sample Input:
+73
+67
+38
+33
+
+
+Sample Output:
+75
+67
+40
+33
+
+    */
+    public class GradingStudents
+    {
+        public GradingStudents()
+        {
+
+        }
+    }
+}
