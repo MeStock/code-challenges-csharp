fcb198197d815b9fc8f44f46b653bd5f0d470140
Melissa Stock <47542442+MeStock@users.noreply.github.com>
2019-10-22 12:50:59 -0700

    Update MagicSquare.cs

--- code-challenges/MagicSquare.cs
+++ code-challenges/MagicSquare.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 namespace code_challenges
 {
     /*
-    We define a magic square to be an  n x x matrix of distinct positive integers from  1 to n^2 where the sum of any row, column, or diagonal of length n is always equal to the same number: the magic constant.
+    We define a magic square to be an  n x x matrix of distinct positive integers from  1 to n^2 
+    where the sum of any row, column, or diagonal of length n is always equal 
+    to the same number: the magic constant.
 
-    You will be given a 3x3 matrix s of integers in the inclusive range [1,9]. We can convert any digit a to any other digit b in the range [1,9] at cost of |a-b|. Given s, convert it into a magic square at minimal cost. Print this cost on a new line.
+    You will be given a 3x3 matrix s of integers in the inclusive range [1,9]. 
+    We can convert any digit a to any other digit b in the range [1,9] at cost of |a-b|. 
+    Given s, convert it into a magic square at minimal cost. Print this cost on a new line.
 
     Note: The resulting magic square must contain distinct integers in the inclusive range [1,9].
 
