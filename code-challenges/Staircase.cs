73d95450d3895efb6e1a7130da621dd068852504
Melissa Stock <melissastock44@gmail.com>
2019-10-22 12:48:39 -0700

    solved hackerrank staircase problem

---
+++ code-challenges/Staircase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace code_challenges
+{
+    public class Staircase
+    {
+        static void staircase(int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int spaces = n - row - 1; spaces > 0; spaces--)
+                {
+                    Console.Write(' ');
+                }
+                for (int hashtags = 0; hashtags <= row; hashtags++)
+                {
+                    Console.Write('#');
+                }
+                if (row != n - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
