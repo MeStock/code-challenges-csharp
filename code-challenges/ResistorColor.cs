a2f74848d2974cd76e6f35ce45d5ee63b5de3027
Melissa Stock <melissastock44@gmail.com>
2019-10-24 15:46:36 -0700

    solved exercism resistor color problem

---
+++ code-challenges/ResistorColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace code_challenges
+{
+    public static class ResistorColor
+    {
+        public static int ColorCode(string color)
+        {
+            int selectedColor = Array.IndexOf(Colors(), color);
+            return selectedColor;
+        }
+
+        public static string[] Colors()
+        {
+            string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+            return colors;
+        }
+    }
+}
