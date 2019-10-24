b8ccbbd44ea4cfbc0883c1e91216747903722db2
Melissa Stock <melissastock44@gmail.com>
2019-10-24 15:47:35 -0700

    solved exercism gigasecond problem

---
+++ code-challenges/Gigasecond.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace code_challenges
+{
+    public static class Gigasecond
+    {
+        public static DateTime Add(DateTime moment)
+        {
+            int gigsec = 1000000000;
+            var duration = new System.TimeSpan(0, 0, 0, gigsec);
+            return moment.Add(duration);
+        }
+    }
+}
