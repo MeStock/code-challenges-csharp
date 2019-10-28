ca559d97e40ab62696ba36a95af9ca2a9a4faf4b
Melissa Stock <melissastock44@gmail.com>
2019-10-28 15:13:19 -0700

    solved exercism Strain problem

---
+++ code-challenges/Strain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code_challenges
+{
+    public static class Strain
+    {
+        public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+
+            foreach (T item in collection)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+
+            foreach (T item in collection)
+            {
+                if (!predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+    }
+}
