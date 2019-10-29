c33ad3729643e00aa73e1b429479f581f2db28d9
Melissa Stock <melissastock44@gmail.com>
2019-10-29 15:19:32 -0700

    solved exercism sum of multiples problem

---
+++ code-challenges/SumOfMultiples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code_challenges
+{
+    public static class SumOfMultiples
+    {
+        public static int Sum(IEnumerable<int> multiples, int max)
+        {
+            int sum = 0;
+            for (int i = 1; i < max; i++)
+            {
+                bool isMultiple = false;
+                foreach (int number in multiples)
+                {
+                    if (number == 0) continue;
+                    if (i % number == 0) isMultiple = true;
+                }
+                if (isMultiple) sum += i;
+            }
+            return sum;
+        }
+    }
+}
