a3630a8e477cf50c0f4210ecc29afd55875e8d92
Melissa Stock <melissastock44@gmail.com>
2019-10-29 15:17:14 -0700

    solved exercism reverse string problem

---
+++ code-challenges/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace code_challenges
+{
+    public static class ReverseString
+    {
+        public static string Reverse(string input)
+        {
+            int middlePoint = input.Length / 2;
+            char[] result = input.ToCharArray();
+            for (int i = 0; i < middlePoint; i++)
+            {
+                char temp = result[i];
+                result[i] = result[result.Length - 1 - i];
+                result[result.Length - 1 - i] = temp;
+            }
+            return new string(result);
+        }
+    }
+}
