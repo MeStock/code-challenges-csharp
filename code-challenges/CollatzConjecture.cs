ce57abf958f51a35294c017cdcb604d0c777ecfb
Melissa Stock <melissastock44@gmail.com>
2019-10-29 15:23:18 -0700

    solved exercism collatz conjecture problem

---
+++ code-challenges/CollatzConjecture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace code_challenges
+{
+    public static class CollatzConjecture
+    {
+        public static int Steps(int number, int numberOfSteps = 0)
+        {
+            if (number < 1) throw new ArgumentException();
+            if (number == 1) return numberOfSteps;
+
+            if (number % 2 == 0) number = number / 2;
+            else number = number * 3 + 1;
+            return Steps(number, numberOfSteps + 1);
+        }
+    }
+}
