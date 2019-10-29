3e27daf66e53af9f10fae32fc15a91d440e47804
Melissa Stock <47542442+MeStock@users.noreply.github.com>
2019-10-29 15:31:23 -0700

    Update BreakingRecords.cs

--- code-challenges/BreakingRecords.cs
+++ code-challenges/BreakingRecords.cs
@@ -3,8 +3,24 @@ namespace code_challenges
 {
     public class BreakingRecords
     {
-        public BreakingRecords()
-        {
+        static int[] breakingRecords(int[] scores) {
+            int currentMin = scores[0];
+            int currentMax = scores[0];
+            int numOfMinChanges = 0;
+            int numOfMaxChanges = 0;
+            for(int i = 1; i < scores.Length; i++)
+            {
+                if(scores[i] < currentMin)
+                {
+                    currentMin = scores[i];
+                    numOfMinChanges++;
+                }else if(scores[i] > currentMax)
+                {
+                    currentMax = scores[i];
+                    numOfMaxChanges++;
+                }
+            }
+            return new int[] {numOfMaxChanges, numOfMinChanges};
         }
     }
 }
