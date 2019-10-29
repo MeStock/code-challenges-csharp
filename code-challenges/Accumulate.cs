ba4475e4c41e38816b01f77f5b1946233a5db776
Melissa Stock <melissastock44@gmail.com>
2019-10-29 15:21:57 -0700

    solved exercism accumulate problem

---
+++ code-challenges/Accumulate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code_challenges
+{
+    public static class AccumulateExtensions
+    {
+        public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
+        {
+            foreach (T item in collection)
+            {
+                yield return func(item);
+            }
+        }
+    }
+}
